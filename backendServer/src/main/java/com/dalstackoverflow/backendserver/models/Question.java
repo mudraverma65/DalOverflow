3ce4201bd78a470fe99422fb823013fb48da374d
Sreejith Nair <sr816885@dal.ca>
2023-02-26 17:28:35 -0400

    DALO-60 : Adding the question model and DALOUtility objects.

---
+++ backendServer/src/main/java/com/dalstackoverflow/backendserver/models/Question.java
@@ -0,0 +1,39 @@
+package com.dalstackoverflow.backendserver.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+/**
+ * @author Sreejith Nair
+ * This is the model class for Question table.
+ * The model class will be used for posting and fetching questions from the UI.
+ */
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "question")
+public class Question {
+    /**
+     * This the PK of the table and is auto incremented.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "q_id")
+    private String questionID;
+
+    @NonNull
+    @Column(name = "u_id")
+    private int userID;
+
+    @Column(name = "question_title")
+    private String questionTitle;
+
+    @Column(name = "q_description")
+    private String questionDescription;
+
+}
---
+++ backendServer/src/main/java/com/dalstackoverflow/backendserver/utils/DALOUtility.java
@@ -0,0 +1,8 @@
+package com.dalstackoverflow.backendserver.utils;
+
+/**
+ * @author Sreejith Nair
+ * This is the general utility class for writing utility methods to be used by entire backendServices.
+ */
+public class DALOUtility {
+}
