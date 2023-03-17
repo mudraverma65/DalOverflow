b1fe2ab957d13ceb6bfd034f3d83c2c53afc0e0b
saumyab <sm728522@dal.ca>
2023-03-17 00:45:14 -0300

    Add: Added input validations for Login/Signup

---
+++ react-frontend/src/LoginPage.js
@@ -0,0 +1,160 @@
+import styled from "styled-components";
+import axios from "axios";
+import { Component } from "react";
+import './styles.css';
+import { Navigate } from "react-router-dom";
+
+const Container = styled.h1`
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+    margin-top: 10px;
+    margin-left: 10px;
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+    margin-top: 10px;
+    margin-left: 10px;
+`;
+
+const UsernameInput = styled.input`
+  background: none;
+  padding: 7px;
+  width: 70%;
+  color: #fff;
+  border: 2px solid #aaa;
+  display: block;
+  box-sizing: border-box;
+  margin-left: 10px;
+`;
+
+const PasswordInput = styled.input`
+  background: none;
+  padding: 7px;
+  width: 70%;
+  color: #fff;
+  border: 2px solid #aaa;
+  display: block;
+  box-sizing: border-box;
+  margin-left: 10px;
+`;
+
+const SubmitButton = styled.button`
+  font-size: 1.1rem;
+  color: #fff;
+  border: 0;
+  background-color: #378ad3;
+  border-radius: 5px;
+  text-decoration: none;
+  margin-top: 10px;
+  margin-left: 10px;
+  background: none;
+  padding: 7px;
+  width: 70%;
+  color: #aaa;
+  border: 2px solid #aaa;
+  display: block;
+  box-sizing: border-box;
+  margin-top:30px;
+  margin-left: 10px;
+`;
+
+const Fieldlabel = styled.label`
+    padding: 10px;
+`;
+
+
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: '',
+      password: '',
+      errors: {}
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    const { userName, password} = this.state;
+    // Check for errors
+    let errors = {};
+    if (userName === '') {
+      errors.name = 'Name is required';
+    } 
+    if (password === '') {
+      errors.password = 'Password is required';
+    }
+    if (Object.keys(errors).length === 0) {
+      // Submit the form
+      console.log('Submitting form...');
+      axios.post("http://127.0.0.1:8080/user/login", {
+        username: this.state.userName,
+        password: this.state.password,
+      })
+        .then((response) => {
+        localStorage.setItem("userId", response.data.userId);
+        this.setState({ redirectToHomePage: true });
+      })
+        .catch((error) => {
+        if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+          } else {
+            console.log("Error", error.message);
+                  }alert("Invalid username or password");
+                });
+              } else {
+                // Update state with errors
+                this.setState({ errors });
+              }
+            }
+            componentWillUnmount() {
+              localStorage.removeItem("userId");
+            }
+            render(){
+              const { userName, password, errors } = this.state;
+              return(<>
+                        {this.state.redirectToHomePage && (<Navigate to = {'/'} /> )}
+                        <form onSubmit={this.handleSubmit}>
+                        <Container><h1>Log In</h1></Container>
+                        <br />
+                        <Fieldlabel htmlFor="name">Please enter your username:</Fieldlabel>
+                        <div>
+                            <UsernameInput
+                            placeholder="Username"
+                            type="text"
+                            id="userName"
+                            name="userName"
+                            value={userName}
+                            onChange={this.handleChange}
+                            />
+                            {errors.name && <div className="error">{errors.name}</div>}
+                        </div>
+                        <br />
+                        <Fieldlabel htmlFor="password">Please enter your password:</Fieldlabel>
+                        <div>
+                            <PasswordInput
+                            placeholder="Password"
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={password}
+                            onChange={this.handleChange}
+                            />
+                            {errors.password && <div className="error">{errors.password}</div>}
+                        </div>
+                        <SubmitButton type="submit">Submit</SubmitButton>
+                        </form>
+                      </>
+                      );
+                    }
+                  }
+export default LoginPage;
+
---
+++ react-frontend/src/QuestionList.js
@@ -0,0 +1,104 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
+import QuestionAnswer from "./QuestionAnswer";
+
+const QuestionStat = styled.div`
+  text-align: center;
+  display: inline-block;
+  font-size: 1.2rem;
+  color:#7C7C7C;
+  margin-top:7px;
+  span{
+    font-size:.7rem;
+    display: block;
+    font-weight: 300;
+    margin-top: 4px;
+  }
+  border: 1px #BDA115 solid;
+  border-radius: 8px;
+  margin-right: 2px;
+`;
+
+// const QuestionList = styled.div`
+//     background-color: rgba(255,255,255,.100);
+//     display: grid;
+//     padding: 15px 25px;
+//     border-top: 1px solid #999;
+// `;
+
+const QuestionTitleArea = styled.div`
+  padding: 0 30px;
+`;
+
+const QuestionLink = styled(Link)`
+  text-decoration: none;
+  color:#BDA115;
+  font-size: 1.1rem;
+  display: block;
+  margin-bottom: 5px;
+`;
+
+const StyledQuestionRow = styled.div`
+  background-color: #000000;
+  padding: 15px 15px 10px;
+  display: grid;
+  grid-template-columns: repeat(3, 50px) 1fr;
+  border-top: 1px solid #555;
+`;
+
+const WhoAndWhen = styled.div`
+  display: inline-block;
+  color:#aaa;
+  font-size: .8rem;
+  float:right;
+  padding: 10px 0;
+`;
+
+const Tag = styled.span`
+  display: inline-block;
+  margin-right: 5px;
+  background-color: #222222;
+  color:#FBFFFE;
+  padding: 7px;
+  border-radius: 4px;
+  font-size: .9rem;
+  text-decoration: none;
+  transition: all .2s ease;
+  &:hover{
+    background-color: #7C7C7C;
+    color:#000000;
+  }
+`;
+
+function QuestionList(props) {
+    console.log("Question List Props:"+props);
+    const { question } = props;
+    if (!question) {
+        return <div>Loading...</div>;
+    }
+    if (question.length === 0) {
+        return <div>No questions found.</div>;
+    } else if (question.error) {
+        return <div>Error retrieving questions.</div>;
+    }
+
+    return (
+        <>
+            <StyledQuestionRow key={question.questionID}>
+                <QuestionStat>0<span>votes</span></QuestionStat>
+                <QuestionStat>{question.answerCount}<span>answers</span></QuestionStat>
+                <QuestionTitleArea>
+                    <QuestionLink><Link to={"./QuestionAnswer"}>{question.questionTitle}</Link></QuestionLink>
+                    {question.tags.map(tag => (
+                        <Tag key={tag}>{tag}</Tag>
+                    ))}
+                    <WhoAndWhen>Asked by {question.userName} at {question.questionDate}</WhoAndWhen>
+                </QuestionTitleArea>
+            </StyledQuestionRow>
+        </>
+    );
+}
+
+
+  export default QuestionList;
