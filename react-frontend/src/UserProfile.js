abd7ebfa97f54c38ed040b0bf7a206063a59e9a5
saumyab <sm728522@dal.ca>
2023-04-07 18:24:28 -0300

    Add: Added code for user description on profile page

--- react-frontend/src/UserProfile.js
+++ react-frontend/src/UserProfile.js
@@ -11,6 +11,8 @@ const Container = styled.div`
     margin-bottom: 10px;
     margin-top: 10px;
     margin-left: 10px;
+    display: flex;
+    flex-direction: column;
 `;
 
 const Header1 = styled.h1`
@@ -27,7 +29,30 @@ const Button = styled.button`
     text-decoration: none;
     margin-top: 10px;
     margin-left: 10px;
+    display: block;
 `;
+
+const Tile = ({title, content}) => {
+  return (
+    <div className="tile">
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
+};
+
+const TileList = ({tiles}) => {
+  return (
+    <div className="tile-list">
+      {tiles.map(tile => <Tile key={tile.id} title={tile.title} content={tile.content} />)}
+    </div>
+  );
+};
+
+const tiles = [
+  { id: 1, title: 'Tile 1', content: 'This is the content of Tile 1.' },
+  { id: 2, title: 'Tile 2', content: 'This is the content of Tile 2.' }
+];
 
 function UserProfile() {
     const[BackToHomePage, setBackToHomePage] = useState(false);
@@ -44,10 +69,17 @@ function UserProfile() {
             {BackToHomePage && (
                 <Navigate to={'/'}/>
             )}
+            <div>
             <Container>
                 <Header1>{localStorage.getItem("username").charAt(0).toUpperCase()+ localStorage.getItem("username").slice(1)}❜s Profile</Header1>
-                <Button onClick={() => Logout()}>Logout</Button>
+                <br />
+                <TileList tiles={tiles} />
+                <br />
             </Container>
+            </div>
+            <div>
+            <Button onClick={() => Logout()}>Logout</Button>
+            </div>
 
         </>
     );
