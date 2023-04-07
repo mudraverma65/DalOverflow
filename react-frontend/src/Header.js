b81af006ff0fb99398ec96a6f6d8946ae7c67f2b
mudra <mudraverma@dal.ca>
2023-04-07 17:37:22 -0300

    DAL0-99 Header Redesigning

--- react-frontend/src/Header.js
+++ react-frontend/src/Header.js
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLayerGroup } from '@fortawesome/free-solid-svg-icons';
 import {Link} from 'react-router-dom';
@@ -6,43 +5,31 @@ import {useContext} from 'react';
 import UserLoggedIn from './UserLoggedIn';
 import SearchBar from './SearchBar';
 
-const ProfileLink = styled(Link)`
-    background: #FFD401;
-    border-radius: 10px;
-    color: #00000;
-    line-height: 55px;
-    text-decoration: none;
-    display: inline-block;
-    padding: 0 20px;
-    margin-right: 5px;
-    margin-left: 10px;
-
-`;
-
 function Header(){
     const {user} = useContext(UserLoggedIn);
 
     return(
         <div className='jsBeginnerWantToInsertTeParent'>
-            <div className='Header'>
+            <div className='Header' >
                 <div className='LogoCol'>
-                    <Link to={'/'}>
+                    <Link style={{display: 'flex', alignItems: 'center',textDecoration: 'none' }} to={'/'}>
                         <FontAwesomeIcon icon={faLayerGroup} size="3x" color='#ffd401' />
                         <div class='LogoText'><span>DalOverflow</span></div>
                     </Link>
                 </div>
                 <SearchBar/>
                 {user && (
-
-                    <ProfileLink to={'/profile'} className="profile">Hi, {user.userName.charAt(0).toUpperCase()+ user.userName.slice(1)}</ProfileLink>
+                    <div class="button1" >
+                        <Link style={{textDecoration: 'none'}} to={'/profile' } className="profile">Hi, {user.userName.charAt(0).toUpperCase()+ user.userName.slice(1)}</Link>
+                    </div>
                 )}
 
                 {!user && (
-                    <div className='LoginHeader'>
-                        <Link to={'/login'}>
+                    <div className='LoginHeader' >
+                        <Link style={{textDecoration: 'none'}} to={'/login'}>
                             <div class="button1" >Login</div><span> </span>
                         </Link>
-                        <Link to={'/registration'}>
+                        <Link style={{textDecoration: 'none'}} to={'/registration'}>
                             <div class="button1" >Register</div>
                         </Link>    
                     </div>
