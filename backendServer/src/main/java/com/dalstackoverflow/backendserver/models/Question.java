44540bb9009b91fd1ce47f44c6f8eb71af5a5a0c
Sreejith Nair <sr816885@dal.ca>
2023-04-08 19:50:44 -0300

    DALO-119 : Adding test case for fetchTopQuestions and refactoring code

--- backendServer/src/main/java/com/dalstackoverflow/backendserver/models/Question.java
+++ backendServer/src/main/java/com/dalstackoverflow/backendserver/models/Question.java
@@ -32,15 +32,19 @@ public class Question {
     @Column(name = "u_id")
     private int userID;
 
+    @NonNull
     @Column(name = "q_title")
     private String questionTitle;
 
+    @NonNull
     @Column(name = "q_description")
     private String questionDescription;
 
+    @NonNull
     @Column(name="q_code")
     private String questionCode;
 
+    @NonNull
     @Column(name="q_date")
     private String questionDate;
 
@@ -58,6 +62,25 @@ public class Question {
     // this is used to store all instance of all answers
     @Transient
     public List<Answer> allAnswers;
+
+    /**
+     * Creating this constructor to initialize question object for testing.
+     * @param questionID
+     * @param userID
+     * @param questionTitle
+     * @param questionDescription
+     * @param questionCode
+     * @param questionDate
+     */
+    public Question(int questionID, @NonNull int userID, @NonNull String questionTitle, @NonNull String questionDescription, @NonNull String questionCode, @NonNull String questionDate) {
+        this.questionID = questionID;
+        this.userID = userID;
+        this.questionTitle = questionTitle;
+        this.questionDescription = questionDescription;
+        this.questionCode = questionCode;
+        this.questionDate = questionDate;
+        this.tags = new ArrayList<String>();
+    }
 
     public List<Answer> getAllAnswers() {
         return allAnswers;
