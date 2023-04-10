ada13f6310b1cc995caf9df766c5686b646a1325
mudra <mudraverma@dal.ca>
2023-04-09 21:00:27 -0300

    DALO-99 Final Changes

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,9 +1,7 @@
 import { Reset } from 'styled-reset';
-import styled, {createGlobalStyle} from 'styled-components';
 import Header from "./Header";
 import Footer from "./Footer";
 import QuestionsFrontPage from "./QuestionsFrontPage";
-import AskQuestion from "./AskQuestion";
 import {
   BrowserRouter as Router,
   Routes,
@@ -22,19 +20,10 @@ import SearchResult from './SearchResult';
 import Register from './Register';
 import QuestionAsk from './QuestionAsk';
 
-const Styles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;700&display=swap');
-  body{
-    background: #2d2d2d; 
-    color: #fff;
-    font-family: Times New Roman;
-  } 
-`;
 
 function App() {
   const[user, setUser] = useState(null);
   const userId = localStorage.getItem("userId")
-
   function checkUser(){
     axios.get(`http://127.0.0.1:8080/user/login/${userId}`)
     .then(response => {
@@ -45,18 +34,13 @@ function App() {
       setUser(null);
     })
   }
-
-
   useEffect(() => {
     checkUser();
 })
 
-
-
   return (
     <div>
-      <Reset />
-      <Styles />
+      <Reset />  
       <Router>
         <UserLoggedIn.Provider value={{user, checkUser}}>
           <Header/>
@@ -71,8 +55,7 @@ function App() {
             <Route path="/search-result" element = {<SearchResult/>} />
           </Routes >
           <Footer/>
-          </UserLoggedIn.Provider>
-        
+          </UserLoggedIn.Provider> 
       </Router>
 
     </div>
